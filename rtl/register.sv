b6d375992102a61a14a347c937c3a8795b64277a
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-08-01 21:25:23 +0200

    misc: add new vscode and task configuration

--- rtl/register.sv
+++ rtl/register.sv
@@ -1,25 +1,22 @@
 module register #(
-	parameter integer WIDTH = 32;
+    parameter integer WIDTH = 32
 ) (
-	input  clk_i,
-	input  rstn_i,
-	input  clk_gate_i,
-	input  [WIDTH-1:0] data_i,
-	output [WIDTH-1:0] data_o
+    input              clk_i,
+    input              rstn_i,
+    input              clk_gate_i,
+    input  [WIDTH-1:0] data_i,
+    output [WIDTH-1:0] data_o
 );
+  always_comb begin
+    data_o = data_r;
+  end
 
-reg [WIDTH-1:0] data_r;
-
-always_comb begin
-	data_o = data_r;
-end
-
-always_ff @( posedge clk_i or negedge rstn_i ) begin
-	if (!rstn_i) begin
-		data_r <= '0;
-	end else if (!clk_gate_i) begin
-		data_r <= data_i;
-	end
-end
+  always_ff @(posedge clk_i or negedge rstn_i) begin
+    if (!rstn_i) begin
+      data_r <= '0;
+    end else if (!clk_gate_i) begin
+      data_r <= data_i;
+    end
+  end
 
 endmodule
