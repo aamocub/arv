93dc33c804e1565aca5bfd1032f6b6c3e26f4dd4
aamocub <alfonso.alfurtx@gmail.com>
2025-08-27 14:22:20 +0200

    rtl: Fix register bug where internal data reg was not present

--- rtl/register.sv
+++ rtl/register.sv
@@ -2,17 +2,18 @@ module register #(
     parameter integer WIDTH = 32
 ) (
     input              clk_i,
-    input              rstn_i,
+    input              rst_ni,
     input              clk_gate_i,
     input  [WIDTH-1:0] data_i,
     output [WIDTH-1:0] data_o
 );
-  always_comb begin
-    data_o = data_r;
-  end
 
-  always_ff @(posedge clk_i or negedge rstn_i) begin
-    if (!rstn_i) begin
+  reg [WIDTH-1:0] data_r;
+
+  assign data_o = data_r;
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
       data_r <= '0;
     end else if (!clk_gate_i) begin
       data_r <= data_i;
