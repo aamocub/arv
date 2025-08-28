ec89deaad2c7071ecef194337ec487ca7ab081ea
aamocub <aamocub@gmail.com>
2025-08-28 19:08:49 +0200

    rtl: Update register module design with minor changes

--- rtl/register.sv
+++ rtl/register.sv
@@ -1,22 +1,18 @@
 module register #(
     parameter integer WIDTH = 32
 ) (
-    input              clk_i,
-    input              rst_ni,
-    input              clk_gate_i,
-    input  [WIDTH-1:0] data_i,
-    output [WIDTH-1:0] data_o
+    input                  clk_i,
+    input                  rst_i,
+    input                  clk_gate_i,
+    input      [WIDTH-1:0] d_i,
+    output reg [WIDTH-1:0] q_o
 );
 
-  reg [WIDTH-1:0] data_r;
-
-  assign data_o = data_r;
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      data_r <= '0;
+  always_ff @(posedge clk_i or posedge rst_i) begin
+    if (rst_i) begin
+      q_o <= '0;
     end else if (!clk_gate_i) begin
-      data_r <= data_i;
+      q_o <= d_i;
     end
   end
 
